c3d687f9aaae8fb7e731d58a722334ecf0e08ca5
db001 <dnbennett@hotmail.co.uk>
2018-04-10 21:11:08 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Webhook payload URL http://localhost:${PORT}/payload
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./routes/index');
+
+const app = express();
+
+require('dotenv').config();
+
+const secret = process.env.WEBHOOK_SECRET;
+
+app.set('views', path.join(__dirname, 'views')); // Folder containing pug files
+app.set('view engine', 'pug');
+
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+app.set('port', process.env.PORT);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running on port ${server.address().port}`);
+});
