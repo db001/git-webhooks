7aa7bb6fbbc997b5be3a9d0353b94959624d1199
db001 <dnbennett@hotmail.co.uk>
2018-04-10 22:58:12 +0100

    receives webhook data

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,13 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/payload', (req, res) => {
-  console.log(req.body);
-})
+router.post('/payload', (req, res) => {
+  const pushData = req.body.head_commit;
+  if(pushData) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }  
+});
 
 module.exports = router;
