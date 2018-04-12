c892b86977ba357a7a2c80ba945b03f26d3c7750
db001 <dnbennett@hotmail.co.uk>
2018-04-12 12:58:39 +0100

    removed controller file, all logic in routes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const hookController = require('../controllers/hookController');
+// const hookController = require('../controllers/hookController');
 
 router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.post('/payload', (req, res, next) => {
-  hookController.getWebhookData
-  // hookController.renderData
+router.post('/payload', (req, res) => {
+  const pushData = req.body;
+  console.log(pushData);
+  res.render('payload', {
+    data: pushData
+  });
 });
 
 module.exports = router;
