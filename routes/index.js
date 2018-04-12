ac57469fc9b963adebc41df20cb8f31675828f88
db001 <dnbennett@hotmail.co.uk>
2018-04-12 13:12:15 +0100

    Renders to screen usingglobal variable

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,21 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+let pushData;
+
 router.post('/payload', (req, res) => {
-  const pushData = req.body;
-  console.log(pushData);
+  pushData = JSON.stringify(req.body);
+  if(req) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+router.get('/payload', (req, res) => {
   res.render('payload', {
     data: pushData
   });
-});
+})
 
 module.exports = router;
